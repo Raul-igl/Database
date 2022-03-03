ebcf6ecbea8efd0c6f3192dec4b702c9844711e0
Raul Iglesias Santos <r0832685@student.thomasmore.be>
2022-03-03 08:32:02 +0100

    .

--- Scraper/Scraper.py
+++ Scraper/Scraper.py
@@ -10,49 +10,51 @@ import requests
 import csv
 import pandas as pd
 
-req = requests.get("https://www.blockchain.com/btc/unconfirmed-transactions")
-soup = BeautifulSoup(req.text, features="html.parser")
-all = soup.findAll('div' , attrs={'class' :"sc-1g6z4xm-0 hXyplo"})
+while True:
+    req = requests.get("https://www.blockchain.com/btc/unconfirmed-transactions")
+    soup = BeautifulSoup(req.text, features="html.parser")
+    all = soup.findAll('div' , attrs={'class' :"sc-1g6z4xm-0 hXyplo"})
 
-listcoins = []
-coin = []
-
-for i in range(0,48):
-    tekst = all[i].text
-
-    splitup = tekst.split('Time')
-
-    hash = splitup[0]
-    hash = hash.replace("Hash", "")
-    Rest = splitup[1]
-    Time = Rest[0:5]
-    money = Rest[17:]
-    splitup2 = money.split('BTCAmount')
-    btc = splitup2[0]
-    usd = splitup2[1]
-    usd = usd.replace("(USD)$", "")
-    usd = usd.replace(",", "")
-    double(btc)
-    double(usd)
-
-    coin.append(hash)
-    coin.append(Time)
-    coin.append(btc)
-    coin.append(usd)
-
-    listcoins.append(coin)
+    listcoins = []
     coin = []
 
-head = ['Hash', 'Time', 'BTC' , 'USD']
-with open('sorted.csv', 'w', encoding='UTF8') as filewrite:
-    write = csv.writer(filewrite)
-    write.writerow(head)
-    write.writerows(listcoins)
+    for i in range(0,48):
+        tekst = all[i].text
 
-dataf = pd.read_csv("sorted.csv")
-dataf.sort_values(["BTC"], axis=0, ascending=[False], inplace=True)
+        splitup = tekst.split('Time')
 
-print(dataf)
+        hash = splitup[0]
+        hash = hash.replace("Hash", "")
+        Rest = splitup[1]
+        Time = Rest[0:5]
+        money = Rest[17:]
+        splitup2 = money.split('BTCAmount')
+        btc = splitup2[0]
+        usd = splitup2[1]
+        usd = usd.replace("(USD)$", "")
+        usd = usd.replace(",", "")
+        double(btc)
+        double(usd)
+
+        coin.append(hash)
+        coin.append(Time)
+        coin.append(btc)
+        coin.append(usd)
+
+        listcoins.append(coin)
+        coin = []
+
+    head = ['Hash', 'Time', 'BTC' , 'USD']
+    with open('sorted.csv', 'w', encoding='UTF8') as filewrite:
+        write = csv.writer(filewrite)
+        write.writerow(head)
+        write.writerows(listcoins)
+
+    dataf = pd.read_csv("sorted.csv")
+    dataf.sort_values(["BTC"], axis=0, ascending=[False], inplace=True)
+
+    print(dataf)
+    time.sleep(60)
 
 
 
